d2e43147f740f2fb45b492f96da3ff732d9b644c
Your Name <phamtienquan2001@gmail.com>
2022-11-16 18:03:06 +0700

    overflow

--- Fe/src/layouts/MainLayout.jsx
+++ Fe/src/layouts/MainLayout.jsx
@@ -10,12 +10,10 @@ const MainLayout = () => {
   return (
     <Row xl={12} sm={12} md={12} xs={12} lg={12} xxl={12}>
       <Col xl={12} sm={12} md={12} xs={12} lg={12} xxl={12}>
-
           <Layout>
             <Row xl={12} sm={12} md={12} xs={12} lg={12} xxl={12}>
               <Col xl={12} sm={12} md={12} xs={12} lg={12} xxl={12}>
-                <Header style={{}}>
-
+                <Header>
                 </Header>
               </Col>
             </Row>
@@ -26,7 +24,6 @@ const MainLayout = () => {
             </Content>
             <Footer style={{textAlign: 'center',background:"black",color:"white"}}
                     className=" site-layout-background">
-              hahah
             </Footer >
           </Layout>
 
