14a4c1c28578ef962c7d8be92f234468bf371009
Your Name <phamtienquan2001@gmail.com>
2022-11-16 13:58:43 +0700

    setup redux

--- src/index.jsx
+++ src/index.jsx
@@ -5,16 +5,24 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import {QueryClient, QueryClientProvider} from "react-query";
+import {Provider} from "react-redux";
+import {persistor, store} from "./store/Store";
+import {PersistGate} from "redux-persist/integration/react";
+import Loading from "./components/loading/LoadingComponent";
 
 const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </QueryClientProvider>
+  <Provider store={store}>
+    <PersistGate loading={<Loading/>} persistor={persistor}>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
