20cff2c223e7d5d57e370bcbe74f7908d0ef5062
ThanhSon232 <88186817+ThanhSon232@users.noreply.github.com>
2022-12-06 21:48:46 +0700

    fix

--- backend/src/main/java/com/example/backend/model/request/RemoveRequest.java
+++ backend/src/main/java/com/example/backend/model/request/RemoveRequest.java
@@ -3,7 +3,7 @@ package com.example.backend.model.request;
 import lombok.Data;
 
 @Data
-public class RemoveMemberRequest {
+public class RemoveRequest {
     private String gmail;
     private String nameRoom;
 }
