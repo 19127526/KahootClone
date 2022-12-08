18ca98b2a5f166f8dca989aa7e167ae72c8f7f8e
huy <69897697+galindo-hoang@users.noreply.github.com>
2022-12-08 20:44:23 +0700

    .

--- src/main/java/com/example/backend/common/configure/WebConfig.java
+++ src/main/java/com/example/backend/common/configure/WebConfig.java
@@ -10,7 +10,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
-//        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedOrigins("https://kahoot-clone-zwc3-git-mainv2-19127526.vercel.app/")
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTION")
+                .allowCredentials(true);
     }
 }
