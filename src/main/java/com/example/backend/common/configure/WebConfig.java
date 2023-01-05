0502bf574169becbcf3a8e74aaf46d4490695c24
huy hoang <hmhuy191101@gmail.com>
2023-01-05 23:06:24 +0700

    add domain

--- src/main/java/com/example/backend/common/configure/WebConfig.java
+++ src/main/java/com/example/backend/common/configure/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://kahoot-clone-j165-d6y08g18r-19127526.vercel.app/")
+                .allowedOrigins("https://kahoot-clone-j165-d6y08g18r-19127526.vercel.app/","https://kahoot-clone-j165-ipgerm1j3-19127526.vercel.app/presentation/")
                 .allowedMethods("GET","POST","PUT","DELETE","HEAD","OPTION")
                 .allowCredentials(true);
     }
